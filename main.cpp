729334b7bbc5e44425bfa24fdc770ae31aaa7027
nixon <nixonmartinezvalencia@gmail.com>
2020-04-20 12:03:27 -0500

    agregando proyecto c++ a git

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	float numero,resultado;
+	
+	cout<<"ingrese un numero flotante"<<endl;
+	cin>>numero;
+	
+	resultado = numero /100;
+	
+	cout<<"el resultado de la division entre 100 es : "<<resultado<<endl;
+	
+	
+	
+	
+	
+	return 0;
+}
